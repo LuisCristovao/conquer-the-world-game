2f8050a4b6698825889e40c3e69703e8e5df2b50
LuisCristovao <luis.cristovao92@gmail.com>
2018-07-20 23:21:21 +0100

    working but not in a general way

--- ctwg.js
+++ ctwg.js
@@ -29,16 +29,33 @@ function Menu(question,options){
     textarea.innerHTML=question+'\n';
     for(i=0;i<options.length;i++){
         op=options[i];
-        textarea.innerHTML+=i.toString()+op+'\n';
+        textarea.innerHTML+=i.toString()+'. '+op+'\n';
     }
     textarea.innerHTML+="Choose option: \n";
-    if(op_selected>=0 && op_selected<options.length){
+    if(op_selected>=0 && op_selected<options.length && op_selected!=''){
         textarea.innerHTML+=op_selected+'\n';
         textarea.innerHTML+='Valid Input, Go to next menu...';
         return op_selected;
     }
     else{
+        //textarea.innerHTML+='Invalid Input\n';
+        //setTimeout(function(){},1000);
         return -1;
+    }
+}
+/*------------------------------------------------
+func: is the function you want class action to do
+params: is an array with parameters for the function
+--------------------------------------------------*/
+class ActionMenu{
+    
+    constructor(func,question,options){
+        this.func=func;
+        this.question=question;
+        this.options=options
+    }
+    run(){
+        return parseInt(this.func(this.question,this.options));
     }
 }
 /*-----------------------------------------------
@@ -46,70 +63,93 @@ action: is a function that does an action and return a value, could be from an i
 next_state: is a function that evaluates the value return from action and returns the next state value.
 ------------------------------------------------*/
 class State{
-    Constructor(action,next_state){
+    constructor(action,next_state){
         this.action=action;
         this.next=next_state;
     }
-    Run(){
-        this.next(this.action());
+    run(){
+        return this.next(this.action.run());
     }
 }
 
 function Game() {
     
     if(value!=null){
+        console.log(value);
         textarea.innerHTML="";
         actual_state=states[value];
-        value=actual_state.Run();
+        value=actual_state.run();
     }
     //console.log(op_selected);
     window.requestAnimationFrame(Game);
 }
 //Main____________________________________
 //Create States
-state_initial= new State(function(){Menu(
+state_initial= new State(new ActionMenu(function(question,options){return Menu(question,options);},
 "Menu Initial\n Question is A real?",['yes','no','dont know']
-);},function(value){
+),function(value){
     switch(value){
         case 0: 
+            op_selected=-1;
             return 'init yes answer';
+            
             break;
         case 1:
-            return 'init no answer';
+            op_selected=-1;
+            return 'init no answer';            
             break;
         case 2: 
-            return 'init dont know';
+            op_selected=-1;
+            return 'init dont know';            
             break;
-        default: 
+        default:
+            op_selected=-1;
             return "initial";
     }
 });
-state_init_yes_anwser= new State(Menu(
+state_init_yes_anwser= new State(new ActionMenu(function(question,options){return Menu(question,options);},
 "Menu 1Yes\n Why A is real?",['because i believe','because is scientificly proven','dont know']
 ),function(value){
     switch(value){
         case 0: 
+            op_selected=-1;
             return 'believe answer';
             break;
         case 1:
+            op_selected=-1;
             return 'scientific';
             break;
-        case 2: 
+        case 2:
+            op_selected=-1;
             return 'yes dk';
             break;
         default: 
+            op_selected=-1;
             return "init yes answer";
     }
 });
-state_believe= new State(Menu(
-"Ok you are a believer",[]
+state_believe= new State(new ActionMenu(function(question,options){return Menu(question,options);},
+"Ok you are a believer",['Press this option to continue...']
 ),function(value){
-    return "Final";
+    if(value>=0){
+        
+        op_selected=-1;
+        return "Final";
+    }
+    else{
+        return "believe answer";
+    }
 });
-state_final= new State(Menu(
-"The End",[]
-),function(value){
-    return null;
+state_final= new State(new ActionMenu(function(question,options){return Menu(question,options);},
+"The End",['Press this option to continue...']),function(value){
+    if(value>=0){
+        
+        op_selected=-1;
+        return "initial";
+    }
+    else{
+        return "Final";
+    }
 });
 //insert created states in dictionary
 states['initial']=state_initial;
@@ -118,16 +158,17 @@ states['believe answer']=state_believe;
 states['Final']=state_final;
 //start state machine
 var actual_state=states['initial'];
-var value=actual_state.Run();
+var value=actual_state.run();
 /*class A{
-    constructor(f1){
+    constructor(f1,param){
         this.f=f1;
+        this.param=param;
     }
-    run(value){
-        this.f(value);
+    run(){
+        this.f(this.param);
     }
 }
 
-bla=new A(function(value){alert("ola "+value);})
-bla.run('tiago');*/
+bla=new A(function(value){alert("ola "+value);},'tiago');
+bla.run();*/
 window.requestAnimationFrame(Game);
